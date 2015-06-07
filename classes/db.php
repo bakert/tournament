c050575625952870102e0e1f4441c1af99cfd8cf
Thomas David Baker <bakert@gmail.com>
2015-06-07 15:01:56 -0700

    Nicer interface for getting a single value from the database.

--- classes/db.php
+++ classes/db.php
@@ -3,27 +3,30 @@
 require_once(__DIR__ . '/../shared/db.php');
 
 class Db {
-
-  public function read($sql) {
-    $rs = $this->execute($sql);
-    if ($rs === false) {
-        L()->error("Problem in Db with $sql");
-        return null;
-    }
-    return $rs;
-  }
-
-  public function write($sql) {
-    return $this->execute($sql);
-  }
-
-  private function execute($sql) {
-    return db(
+  public function execute($sql) {
+    $result = db(
       $sql,
       C()->databasename(),
       C()->databasehost(),
       C()->databaseusername(),
       C()->databasepassword()
     );
+    if ($result === false) {
+      throw new DatabaseException("Database failure: '$sql'");
+    }
+    return $result;
+  }
+
+  public function value($sql, $default = 'ERROR') {
+    $rs = $this->execute($sql);
+    if (!isset($rs[0][0])) {
+      if ($default === 'ERROR') {
+        $msg = "Asked for value but none present with '$sql'";
+        throw new DatabaseException($msg);
+      } else {
+        return $default;
+      }
+    }
+    return $rs[0][0];
   }
 }
