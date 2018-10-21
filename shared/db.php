403d2fdfc022ed82e7c216d9a4f99d7b3350b52e
Thomas David Baker <bakert@gmail.com>
2018-10-21 23:15:21 +0000

    Use mysqli as old php lib is deprecated.

--- shared/db.php
+++ shared/db.php
@@ -26,11 +26,11 @@ define('MAXLEN_TEXT', 65535);
  */
 function db($sql, $db="bluebones", $host="localhost", $usr="root",
         $pwd="sleague", $tz=null) {
-    mysql_connect($host, $usr, $pwd);
-    if ($tz) { mysql_query("SET time_zone = '$tz'"); }
-    mysql_select_db($db) or die("Unable to select database: " . $db);
-    $res = parse_result(mysql_query($sql));
-    mysql_close();
+    $conn = mysqli_connect($host, $usr, $pwd);
+    if ($tz) { mysqli_query($conn, "SET time_zone = '$tz'"); }
+    mysqli_select_db($conn, $db) or die("Unable to select database: " . $db);
+    $res = parse_result(mysqli_query($conn, $sql));
+    mysqli_close($conn);
     return $res;
 }
 
@@ -48,7 +48,7 @@ function parse_result($res) {
     }
     $arr = array();
     $i = 0;
-    while ($row = mysql_fetch_array($res)) {
+    while ($row = mysqli_fetch_array($res)) {
         $arr[$i++] = $row;
     }
     return $arr;
@@ -64,7 +64,6 @@ function quote_it($s) {
     if ($s === 'NULL') {
         return 'NULL';
     }
-    //TODO or use mysql_real_escape_string
     return "'" . addslashes($s) . "'";
 }
 
@@ -78,32 +77,59 @@ function sql_date($date) {
     return quote_it(date('Y-m-d H:i:s', $date));
 }
 
-//TODO docs for these three.
+/**
+ * Starts a transaction.
+ *
+ * @param   $db     MySQL database to attach to.
+ * @param   $host   Host to attach to.
+ * @param   $usr    Username for db.
+ * @param   $pwd    Password for db.
+ * @return          Connection object suitable for passing in to transact_db,
+ *                  commit and rollback.
+ */
 function start_transaction($db="bluebones", $host="localhost", $usr="root",
         $pwd="sleague") {
-    $conn = mysql_connect($host, $usr, $pwd);
-    @mysql_select_db($db) or die("Unable to select database: " . $db);
-    mysql_query("START TRANSACTION");
+    $conn = mysqli_connect($host, $usr, $pwd);
+    mysqli_select_db($conn, $db) or die("Unable to select database: " . $db);
+    mysqli_query($conn, "START TRANSACTION");
     return $conn;
 }
 
+/**
+ * Performs a SQL query in an open transaction.
+ *
+ * @param $sql  Query to perform.
+ * @param $conn Transaction connection (from start_transaction).
+ * @return      Results of SQL query (false for failed, number of affected rows
+ *              for a write, array of results for a read).
+ */
 function transact_db($sql, $conn) {
     if (! $conn) {
         return false;
     }
-    return parse_result(mysql_query($sql, $conn));
+    return parse_result(mysqli_query($conn, $sql));
 }
 
+/**
+ * Commit the transaction represented by $conn.
+ *
+ * @param $conn Transaction connection (from start_transaction).
+ * @return      Result of COMMIT.
+ */
 function commit($conn) {
-    $res = mysql_query("COMMIT", $conn);
-    mysql_close($conn);
+    $res = mysqli_query($conn, "COMMIT");
+    mysqli_close($conn);
     return $res;
 }
 
+/**
+ * Rollback the transaction represented by $conn.
+ *
+ * @param $conn Transaction connection (from start_transaction).
+ * @return      Result of ROLLBACK.
+ */
 function rollback($conn) {
-    $res = mysql_query("ROLLBACK", $conn);
-    mysql_close($conn);
+    $res = mysqli_query($conn, "ROLLBACK");
+    mysqli_close($conn);
     return $res;
 }
-
-?>
